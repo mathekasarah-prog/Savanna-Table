d7e03cefa4ce947029b3b62e4a66f0bb4a64cd3c
Sarah <mathekasarah@gmail.com>
2026-09-07 09:57:02 +0800

    Fix guest number selector and enhance booking form submission logging

--- script.js
+++ script.js
@@ -5,13 +5,29 @@ hamburger.addEventListener('click', function() {
         navLinks.classList.toggle("show");
 });
 
-const guests = Number(document.querySelector("#guests").value);
+const guests = Number(document.querySelector("#guests-number").value);
 
 
 const bookingForm = document.querySelector("#booking-form");
 bookingForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log('Form submitted');
+     const fullNameInput = document.querySelector("#full-name");
+     const emailInput = document.querySelector("#email");
+     const phoneInput = document.querySelector("#phone");
+     const guestsInput = document.querySelector("#guests-number");
+     const dateInput = document.querySelector("#reservation-date");
+     const timeInput = document.querySelector("#reservation-time");
+     const seatingInput = document.querySelector("#seating");
+     const specialRequestsInput = document.querySelector("#special-requests");
+
+    console.log(fullNameInput.value);
+    console.log(emailInput.value);
+    console.log(phoneInput.value);
+    console.log(guestsInput.value);
+    console.log(dateInput.value);
+    console.log(timeInput.value);
+    console.log(seatingInput.value);
+    console.log(specialRequestsInput.value);
 });
 
 
